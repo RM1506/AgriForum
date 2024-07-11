fb6987ccd4cbed97bca34290932a1418e94807c7
Rishabh Mishra <rmishrar156@gmail.com>
2024-07-11 19:45:37 +0530

    udated registration validation

--- backend/frontend/src/components/Menu.jsx
+++ backend/frontend/src/components/Menu.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react"
 import { UserContext } from "../context/UserContext"
 import axios from "axios"
-//import { URL } from "../url"
+import toast from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom"
 
 
@@ -15,6 +15,7 @@ const handleLogout=async()=>{
     const res=await axios.get("https://blog-43pq.onrender.com/api/auth/logout",{withCredentials:true})
     // console.log(res)
     setUser(null)
+    toast.success("Logged out successfully");
     navigate("/login")
 
   }
