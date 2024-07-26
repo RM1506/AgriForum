efba82eb9e5de5050d79b9669597e66cbd493fec
Rishabh Mishra <rmishrar156@gmail.com>
2024-07-27 01:35:51 +0530

    update

--- backend/frontend/src/pages/Login.jsx
+++ backend/frontend/src/pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
   }
   return (
     <>
-    <div className="flex items-center justify-between px-6 md:px-[200px] py-4 pt-10  bg-green-800 text-white">
+    <div className="flex items-center justify-between px-6 md:px-[200px] py-4  bg-green-800 text-white">
     <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">AgriForum</Link></h1>
     <h3><Link to="/register">Register</Link></h3>
     </div>
