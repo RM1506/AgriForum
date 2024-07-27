01b934c85d654ffa95983097be1d930ace7651b2
Rishabh Mishra <rmishrar156@gmail.com>
2024-07-27 17:42:44 +0530

    update

--- backend/frontend/src/pages/CreatePost.jsx
+++ backend/frontend/src/pages/CreatePost.jsx
@@ -67,7 +67,7 @@ const CreatePost = () => {
         <h1 className='font-bold md:text-2xl text-xl'>Create a post</h1>
         <form className='w-full flex flex-col space-y-4 md:space-y-8 mt-4'>
           <input onChange={(e) => setTitle(e.target.value)} type="text" placeholder='Enter post title' className='px-4 py-2 outline-none rounded-lg border border-gray-300' />
-          <input onChange={(e) => setFile(e.target.files[0])} type="file" className='px-4' />
+          <input onChange={(e) => setFile(e.target.files[0])} type="file" className='px-4 py-2 outline-none rounded-lg border border-gray-300 bg-white text-black file:py-2 file:px-4 file:bg-black file:text-white' />
           <div className='flex flex-col'>
             <div className='flex items-center space-x-4 md:space-x-8'>
               <input value={cat} onChange={(e) => setCat(e.target.value)} className='px-4 py-2 outline-none rounded-lg border border-gray-300' placeholder='Enter post category' type="text" />
